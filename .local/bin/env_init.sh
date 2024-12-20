c0229dae86b1b5454745eaf7b118fed415a806a0
ericdx <ericdx@uci.edu>
2024-12-19 21:53:51 -0800

    switched starship to plain-text (nerd text not working)

--- .local/bin/env_init.sh
+++ .local/bin/env_init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Setting up environment for ${MSYSTEM}"
 CONFIG_DIR="${1:-$(realpath $(dirname "$0")/../.config)}"
+echo "setting up ssh-agent"
 # add ssh-agent startup to the .bashrc
-# add the ssh key to the .bashrc
 if ! grep -Fxq 'eval "$(ssh-agent -s)"' ~/.bashrc ; then
      echo  >> ~/.bashrc
      echo '#startup of ssh-agent' >> ~/.bashrc
@@ -10,7 +10,9 @@ if ! grep -Fxq 'eval "$(ssh-agent -s)"' ~/.bashrc ; then
      echo 'find ~/.ssh/ | grep -v '\.pub' | grep id | xargs ssh-add' >> ~/.bashrc
 fi
 # install starship (cross-shell prompt)
+echo "setting up starship"
 pacman -Sy mingw-w64-ucrt-x86_64-starship --noconfirm
+pacman -S mingw-w64-ucrt-x86_64-ttf-font-nerd --noconfirm
 mkdir -p ~/.config && cp ${CONFIG_DIR}/starship.toml ~/.config
 # add starship init to the .bashrc
 if ! grep -Fxq 'eval "$(starship init bash)"' ~/.bashrc ; then
@@ -20,7 +22,18 @@ if ! grep -Fxq 'eval "$(starship init bash)"' ~/.bashrc ; then
 	echo 'export STARSHIP_CONFIG=~/.config/starship.toml' >> ~/.bashrc
 fi
 # install The Ultimate vimrc (Basic version)
+echo "setting up .vimrc"
+mkdir -p ~/.config && cp ${CONFIG_DIR}/.vimrc ~/.config
+if ! grep -Fxq '#Ultimate Vimrc' ~/.bashrc ; then
+     echo  >> ~/.bashrc
+     echo '#Ultimate Vimrc' >> ~/.bashrc
+     echo 'alias vim="vim -u ~/.config/.vimrc"' >> ~/.bashrc
+fi
+source ~/.bashrc
 #
 # install/update relevant software (godot, python, pygame)
+#
 
-source ~/.bashrc
+# install/update Python
+echo "setting up python..."
+
