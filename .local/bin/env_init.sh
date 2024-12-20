897527da58e327bd8887dbc5ffec6e96c618f4e5
ericdx <ericdx@uci.edu>
2024-12-19 19:06:55 -0800

    initial starship stuff

--- .local/bin/env_init.sh
+++ .local/bin/env_init.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-echo "Setting up environment for $USER"
-# save previous .bash_profile and .vimrc
-# initialize new .bash_profile and .vimrc
+echo "Setting up environment for ${MSYSTEM}"
+CONFIG_DIR="${1:-$(realpath $(dirname "$0")/../.config)}"
+# install starship (cross-shell prompt)
+pacman -Sy mingw-w64-ucrt-x86_64-starship --noconfirm
+mkdir -p ~/.config && cp ${CONFIG_DIR}/starship.toml ~/.config
+if ! grep -Fxq 'eval "$(starship init bash)"' ~/.bashrc ; then
+	echo >> ~/.bashrc
+	echo '#Starship Initializer' >> ~/.bashrc
+	echo 'eval "$(starship init bash)"' >> ~/.bashrc
+	echo 'export STARSHIP_CONFIG=~/.config/starship.toml' >> ~/.bashrc
+fi
+# install The Ultimate vimrc (Basic version)
+#
 # install/update relevant software (godot, python, pygame)
 
+source ~/.bashrc
--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 USERNAME:= $(git config --global user.name)
 ROOT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 BIN_DIR:= $(ROOT_DIR).local/bin
+CONFIG_DIR:= $(ROOT_DIR).local/.config
 
 .PHONY: all
 .PHONY: initialize_environment
 all:
 	@ echo "Makefile under construction"
 
-initialize_environment:
+setup_environment:
 	@echo "root directory: "$(ROOT_DIR)
 	# delegate to a bash script to setup our environment
-	$(BIN_DIR)/env_init.sh
+	$(BIN_DIR)/env_init.sh $(CONFIG_DIR)
