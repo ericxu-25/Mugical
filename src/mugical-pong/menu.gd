f359e6bf420b176d2e4dc1cc140b757e1ca3faea
chanyoem <chanyoem@uci.edu>
2025-03-11 02:03:05 -0700

    updated gameboard scene to just be main; deleted original game board scene

--- src/mugical-pong/menu.gd
+++ src/mugical-pong/menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_button_pressed() -> void:
 	print("Start button pressed")
-	get_tree().change_scene_to_file("res://Gameboard.tscn")
+	get_tree().change_scene_to_file("res://Main.tscn")
 
 
 func _on_quit_button_pressed() -> void:
